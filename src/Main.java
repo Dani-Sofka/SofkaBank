6b2ad84338022ad02e410358a0552a44d151fa0c
daniel.rua@sofka.com.co <daniel.rua@sofka.local>
2024-12-27 12:09:29 -0500

    Refactor de la clase main

--- src/Main.java
+++ src/Main.java
@@ -11,16 +11,31 @@ public class Main {
         Map<String, BankAccount> bankAccount = new HashMap<>();
 
         BankAccount account1 = new BankAccount("123da", "Daniel Rúa", "savings", status, 100);
-        BankAccount account2 = new BankAccount("123al", "Alejandra Villa", "checking", status,150);
-        BankAccount account3 = new BankAccount("123pa", "Paco Villa", "checking", status,200);
-        BankAccount account4 = new BankAccount("123li", "Lila Villa", "savings", status,50);
+        BankAccount account2 = new BankAccount("123al", "Alejandra Villa", "checking", status,100);
+        BankAccount account3 = new BankAccount("123pa", "Paco Villa", "checking", status,100);
+        BankAccount account4 = new BankAccount("123li", "Lila Villa", "savings", status,100);
 
         bankAccount.put(account1.getId(), account1);
         bankAccount.put(account2.getId(), account2);
         bankAccount.put(account3.getId(), account3);
         bankAccount.put(account4.getId(), account4);
 
-        System.out.println("--------------------------------");
+        displayAccountDetails(bankAccount);
+
+        account1.deposit(150);
+        displayAccountDetails(bankAccount);
+
+        account2.deposit(150);
+        displayAccountDetails(bankAccount);
+
+        account1.withdraw(100);
+        displayAccountDetails(bankAccount);
+
+        account1.closeAccount();
+        displayAccountDetails(bankAccount);
+
+    }
+    private static void displayAccountDetails(Map<String, BankAccount> bankAccount) {
         bankAccount.forEach((id, account) -> {
             System.out.println("Account ID: " + id);
             System.out.println("Account Holder: " + account.getAccountHolder());
@@ -29,46 +44,5 @@ public class Main {
             System.out.println("Status: " + account.getStatus());
             System.out.println("--------------------------------");
         });
-
-        //account2.deposit(0);
-
-        System.out.println("Estado de las cuentas despues del depósito");
-        System.out.println("--------------------------------");
-        bankAccount.forEach((id, account) -> {
-            System.out.println("Account ID: " + id);
-            System.out.println("Account Holder: " + account.getAccountHolder());
-            System.out.println("Account Type: " + account.getAccountType());
-            System.out.println("Balance: " + account.getBalance());
-            System.out.println("Status: " + account.getStatus());
-            System.out.println("--------------------------------");
-        });
-
-        //account1.withdraw(200);
-
-        System.out.println("Estado de las cuentas despues del retiro");
-        System.out.println("--------------------------------");
-        bankAccount.forEach((id, account) -> {
-            System.out.println("Account ID: " + id);
-            System.out.println("Account Holder: " + account.getAccountHolder());
-            System.out.println("Account Type: " + account.getAccountType());
-            System.out.println("Balance: " + account.getBalance());
-            System.out.println("Status: " + account.getStatus());
-            System.out.println("--------------------------------");
-        });
-
-        //account1.closeAccount();
-
-        System.out.println("Estado de las cuentas despues del cierre de cuenta");
-        System.out.println("--------------------------------");
-        bankAccount.forEach((id, account) -> {
-            System.out.println("Account ID: " + id);
-            System.out.println("Account Holder: " + account.getAccountHolder());
-            System.out.println("Account Type: " + account.getAccountType());
-            System.out.println("Balance: " + account.getBalance());
-            System.out.println("Status: " + account.getStatus());
-            System.out.println("--------------------------------");
-        });
-
-
     }
 }
