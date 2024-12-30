68c72a259e2058bd8f3839181ccca56c17d9263c
daniel.rua@sofka.com.co <daniel.rua@sofka.local>
2024-12-30 15:22:25 -0500

    Se implementa patron de diseño Singleton

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import entities.BankAccount;
+import patterns.singleton.AccountManager;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,57 +9,24 @@ public class Main {
 
         String status = "Active";
 
-        Map<String, BankAccount> bankAccount = new HashMap<>();
+        BankAccount account1 = new BankAccount("123da", "Daniel Rúa", "savings", status, 0);
+        BankAccount account2 = new BankAccount("123al", "Alejandra Villa", "checking", status,0);
 
-        BankAccount account1 = new BankAccount("123da", "Daniel Rúa", "savings", status, 100);
-        BankAccount account2 = new BankAccount("123al", "Alejandra Villa", "checking", status,100);
-        BankAccount account3 = new BankAccount("123pa", "Paco Villa", "checking", status,100);
-        BankAccount account4 = new BankAccount("123li", "Lila Villa", "savings", status,100);
+        AccountManager accountManager = AccountManager.getInstance();
 
-        bankAccount.put(account1.getId(), account1);
-        bankAccount.put(account2.getId(), account2);
-        bankAccount.put(account3.getId(), account3);
-        bankAccount.put(account4.getId(), account4);
+        accountManager.addAccount(account1);
+        accountManager.addAccount(account2);
 
         account1.deposit(150);
-        System.out.println("-----------------------------------");
-        System.out.println("Cuentas después del depósito");
-        System.out.println("-----------------------------------");
-        displayAccountDetails(bankAccount);
-
-        account1.deposit(50);
-        System.out.println("-----------------------------------");
-        System.out.println("Cuentas después del depósito");
-        System.out.println("-----------------------------------");
-        displayAccountDetails(bankAccount);
-
         account1.withdraw(100);
-        System.out.println("-----------------------------------");
-        System.out.println("Cuentas después del retiro");
-        System.out.println("-----------------------------------");
-        displayAccountDetails(bankAccount);
-
-        account1.withdraw(0);
-        System.out.println("-----------------------------------");
-        System.out.println("Cuentas después del retiro");
-        System.out.println("-----------------------------------");
-        displayAccountDetails(bankAccount);
-
         account1.closeAccount();
-        System.out.println("-----------------------------------");
-        System.out.println("Cuentas después del cierre");
-        System.out.println("-----------------------------------");
-        displayAccountDetails(bankAccount);
-    }
 
-    private static void displayAccountDetails(Map<String, BankAccount> bankAccount) {
-        bankAccount.forEach((id, account) -> {
-            System.out.println("Account ID: " + id);
-            System.out.println("Account Holder: " + account.getAccountHolder());
-            System.out.println("Account Type: " + account.getAccountType());
-            System.out.println("Balance: " + account.getBalance());
-            System.out.println("Status: " + account.getStatus());
-            System.out.println("--------------------------------");
-        });
+        account2.deposit(150);
+        account2.withdraw(100);
+        account2.closeAccount();
+
+        System.out.println("------------ Cuentas después de las operaciones ------------");
+        accountManager.getAccount("123al");
+        accountManager.displayAccounts();
     }
 }
